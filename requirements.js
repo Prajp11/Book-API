8d9556aa930dc4569d707e2f7cb029ea700e88a2
788986l <pandgale.prajwal11@gmail.com>
2023-03-06 13:09:00 +0530

    API Project 1

---
+++ requirements.js
@@ -0,0 +1,34 @@
+// Requirement for our project
+
+// We are a book management company
+
+// Books
+// ISBN, Title, Publication date, Number of pages, Author[], category[]
+
+
+// Authers
+// Id, name, books[]
+
+// Publications
+// id, name, books[]
+
+// We have to design and code an API over this.
+
+// 1.Books
+// We need an API :-
+// To get all the books - DONE
+// To get specific book - DONE
+// To get a list of books based on category - DONE
+// To get a list of books based on languages
+
+// 2.Authors
+// We need an API :-
+// To get all the authors - DONE
+// To get a specific author
+// To get a list of authors based on books - DONE
+
+// 3.Publications
+// We need an API :-
+// To get all the publications
+// To get a specific publiication
+// To get a list of publications based on book
